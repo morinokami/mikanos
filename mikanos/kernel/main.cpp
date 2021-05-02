55f36cbdd88b703227fb5cdeb9abd94d3be714b8
Shinya Fujino <shf0811@gmail.com>
2021-05-02 12:26:56 +0900

    day5f

--- mikanos/kernel/main.cpp
+++ mikanos/kernel/main.cpp
@@ -17,6 +17,22 @@ void operator delete(void *obj) {
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
 
+char console_buf[sizeof(Console)];
+Console* console;
+
+int printk(const char* format, ...) {
+  va_list ap;
+  int result;
+  char s[1024];
+
+  va_start(ap, format);
+  result = vsprintf(s, format, ap);
+  va_end(ap);
+
+  console->PutString(s);
+  return result;
+}
+
 extern "C" void KernelMain(FrameBufferConfig& frame_buffer_config) {
   switch (frame_buffer_config.pixel_format) {
     case kPixelRGBResv8BitPerColor:
@@ -34,13 +50,11 @@ extern "C" void KernelMain(FrameBufferConfig& frame_buffer_config) {
       pixel_writer->Write(x, y, {255, 255, 255});
     }
   }
-  
-  Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
 
-  char buf[128];
+  console = new(console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
+
   for (int i = 0; i < 27; ++i) {
-    sprintf(buf, "line %d\n", i);
-    console.PutString(buf);
+    printk("printk: %d\n", i);
   }
 
   while (1) __asm__("hlt");
